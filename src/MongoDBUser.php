e8d280b1e1d63df24652f8c9f8a76a49999cb16c
William MARTIN <william.martin@power-lan.com>
2016-12-03 18:01:50 +0100

    Validate inputs

--- src/MongoDBUser.php
+++ src/MongoDBUser.php
@@ -14,11 +14,27 @@ class MongoDBUser extends \photon\storage\mongodb\Object
 
     public function setPassword($pwd)
     {
+        if (is_string($pwd) === false) {
+            throw new \Exception('Password is not a string');
+        }
+
+        if ($pwd === '') {
+            throw new \Exception('Password is empty');
+        }
+
         $this->password = password_hash($pwd, PASSWORD_DEFAULT);
     }
 
     public function verifyPassword($pwd)
     {
+        if (isset($this->password) === false) {
+            return false;
+        }
+
+        if ($this->password === null) {
+            return false;
+        }
+
         $valid = password_verify($pwd, $this->password);
         if ($valid === false) {
             return false;
